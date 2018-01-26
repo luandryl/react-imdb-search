88e3925421da146bc70626a7ff99883c418a21b6
luandryl <luan.andryl@gmail.com>
2018-01-26 15:47:57 -0200

    api search w/ ui to render results

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import { Container, Card, Button, Icon } from 'semantic-ui-react'
+import { Card, Button, Icon } from 'semantic-ui-react'
 import MovieItem from './MovieItem'
+import axios from 'axios'
 import './movies.css'
+
 export default class Movies extends Component {
+  
+  constructor () {
+    super()
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentWillMount = () => {
+    let urlApi = 'http://www.omdbapi.com/?apikey=e75caa77&'
+    axios.get(urlApi + 's=' + this.props.searchString)
+      .then(({data}) => this.setState({movies: data.Search}))
+  }
 
   render () {
+    let cards = this.state.movies.map((m) => {
+      return <MovieItem movie={m}/>
+    })
     return (
       <div className='movies_wrapper'>
         <div className='close_div'>
-          <Button secondary animated='vertical'>
+          <Button secondary animated='vertical' onClick={this.props.closeComponent}>
             <Button.Content hidden>Close</Button.Content>
             <Button.Content visible>
               <Icon name='close' />
@@ -16,8 +34,7 @@ export default class Movies extends Component {
           </Button>
         </div>
         <Card.Group>
-          <MovieItem />
-          <MovieItem />
+          {cards}
         </Card.Group>
       </div>
     );
