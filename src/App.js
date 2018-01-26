7244b238967c39e211e578ad207a479c13de6923
luandryl <luan.andryl@gmail.com>
2018-01-26 15:49:26 -0200

    main component search logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
-import Home from './components/home/Home'
+import React, { Component } from 'react';
+import { Container, Header, Divider, Form, Button } from 'semantic-ui-react'
 import Movies from './components/movies/Movies'
+import './home.css'
 
-export default class App extends Component {
-  render () {
+class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      movieSearch: '',
+      movies: [],
+      isMoviesResults: false
+    }
+  }
+
+  inputHandler = e => { this.setState({movieSearch: e.target.value}) }
+
+  search = () => {
+    if (this.state.movieSearch.length > 0) {
+      this.setState({isMoviesResults: !this.state.isMoviesResults})
+    }
+  }
+
+  closeSearch = () => {this.setState({isMoviesResults: !this.state.isMoviesResults})}
+
+  render() {
     return (
       <div>
-        <Home />
+        {this.state.isMoviesResults === false ? ( 
+          <div className='main_wrapper'>
+            <Header as='h1'> IMDB Search </Header>
+            <Divider />
+            <Container>
+              <Form>
+                <Form.Field>
+                  <input value={this.state.movieSearch} onChange={this.inputHandler} placeholder='Type a Movie title' />
+                </Form.Field>
+                <Button primary type='submit' onClick={this.search}>Go Find</Button>
+              </Form>
+            </Container>  
+          </div>
+        ) : (
+          <Movies searchString={this.state.movieSearch} closeComponent={this.closeSearch}/>
+        )}
       </div>
     );
   }
 }
+
+export default App;
